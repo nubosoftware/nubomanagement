151c51bc49344ec18ae45a5e9987de1700b975d7
Nubo Developer <dev@nubosoftware.com>
2022-03-31 11:33:44 +0300

    Add ssh to docker image

--- scripts/attachEnterpriseMobileModule.sh
+++ scripts/attachEnterpriseMobileModule.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 npm install --only=dev
-if [ ! -d "./nubo-management-mobile" ] 
-then   
+if [ ! -d "./nubo-management-mobile" ]
+then
     echo "Directory nubo-management-mobile does not exists. Clone from git project."
     git clone git@github.com:nubosoftware/nubo-management-mobile.git
 fi
 node ./nubo-management-mobile/scripts/attachToManagement.js
-if [ ! -d "./nubo-management-enterprise" ] 
-then   
+if [ ! -d "./nubo-management-enterprise" ]
+then
     echo "Directory nubo-management-enterprise does not exists. Clone from git project."
     git clone git@github.com:nubosoftware/nubo-management-enterprise.git
 fi
