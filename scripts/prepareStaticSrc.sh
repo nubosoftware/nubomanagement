a9b66f669db5c5ac3ed6bcb843845bbefce6e020
Nubo Developer <dev@nubosoftware.com>
2023-01-10 15:53:40 +0200

    Add npm install to prepare static

--- scripts/prepareStaticSrc.sh
+++ scripts/prepareStaticSrc.sh
@@ -4,9 +4,17 @@ mkdir -p ./static-src
 mkdir -p ./static
 
 cd ./static-src
-git clone https://github.com/nubosoftware/nubo-admin.git
-git clone https://github.com/nubosoftware/nubo-desktop-client.git
 
+# prepare the admin module
+git clone https://github.com/nubosoftware/nubo-admin.git
+cd ./nubo-admin
+npm install
+cd ..
+
+# prepare the desktop client module
+git clone https://github.com/nubosoftware/nubo-desktop-client.git
+cd ./nubo-desktop-client
+npm install
 cd ..
 
 
