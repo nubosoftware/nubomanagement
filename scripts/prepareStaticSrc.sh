ca2fbfa05376d8b7c4113411c50e0a2d37668e01
Nubo Developer <dev@nubosoftware.com>
2023-01-10 14:56:02 +0200

    Fix prepareStaticSrc.sh script

--- scripts/prepareStaticSrc.sh
+++ scripts/prepareStaticSrc.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 mkdir -p ./static-src
-mkir -p ./static
+mkdir -p ./static
 
 cd ./static-src
 git clone https://github.com/nubosoftware/nubo-admin.git
 git clone https://github.com/nubosoftware/nubo-desktop-client.git
+
+cd ..
+
 
